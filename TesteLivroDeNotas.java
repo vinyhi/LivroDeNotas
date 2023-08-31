73fdcce180697ba2644fb1f0ef472a0ff1903bc0
Aluno <aluno@IPIRANGA.FATEC>
2023-08-31 18:02:38 -0300

    ap06(java): teste do livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,8 @@
+public class TesteLivroDeNotas{
+    public static void main(String args []){
+        //variavel de referencia = ponteiro
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+        System.out.println("Ate logo");
+    }
+}
